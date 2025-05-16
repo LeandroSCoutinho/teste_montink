38e81f66e9606105d7d8b7b790c318d1f9557bd0
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2025-05-16 17:24:24 -0300

    Model Estoque

---
+++ models/Estoque.php
@@ -0,0 +1,18 @@
+<?php
+class Estoque {
+    public static function allByProduto($produto_id) {
+        $stmt = DB::getConnection()->prepare("SELECT * FROM estoques WHERE produto_id = ?");
+        $stmt->execute([$produto_id]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public static function create($produto_id, $variacao, $quantidade) {
+        $stmt = DB::getConnection()->prepare("INSERT INTO estoques (produto_id, variacao, quantidade) VALUES (?, ?, ?)");
+        $stmt->execute([$produto_id, $variacao, $quantidade]);
+    }
+
+    public static function update($id, $quantidade) {
+        $stmt = DB::getConnection()->prepare("UPDATE estoques SET quantidade = ? WHERE id = ?");
+        $stmt->execute([$quantidade, $id]);
+    }
+}
